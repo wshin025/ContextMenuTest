1cb67023ddfd48646a0265a944c59470ba4d9db4
wshin025 <wody0506@gmail.com>
2025-06-16 14:21:13 +0900

    저장

--- app/src/main/java/kr/ac/kopo/contextmenutest/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/contextmenutest/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.ContextMenu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -15,8 +17,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     Button btnBg, btnButton;
+    LinearLayout linear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,53 +32,50 @@ public class MainActivity extends AppCompatActivity {
         });
         btnBg = findViewById(R.id.btn_bg);
         btnButton = findViewById(R.id.btn_button);
-        linear =  findViewById(R.id.main);
+        linear = findViewById(R.id.main);
         registerForContextMenu(btnBg);
         registerForContextMenu(btnButton);
-                
-
     }
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
+
         MenuInflater menuInflater = getMenuInflater();
-        if(v == btnBg){
+        if (v == btnBg) {
             menu.setHeaderTitle("배경색 변경");
-            menuInflater.inflate(R.menu.menu_bg,menu);
+            menuInflater.inflate(R.menu.menu_bg, menu);
         }
-        if(v == btnButton){
+
+        if (v == btnButton) {
             menu.setHeaderTitle("버튼 변경");
-            menuInflater.inflate(R.menu.menu_button ,menu);
+            menuInflater.inflate(R.menu.menu_button, menu);
         }
     }
 
     @Override
     public boolean onContextItemSelected(@NonNull MenuItem item) {
-        return super.onContextItemSelected(item);
-        if(item.getItemId() == R.id.item_bg_red){
+        super.onContextItemSelected(item);
+        if (item.getItemId() == R.id.item_bg_red) {
             linear.setBackgroundColor(Color.RED);
             return true;
-        }else if(item.getItemId() == R.id.item_bg_blue){
+        } else if (item.getItemId() == R.id.item_bg_blue) {
             linear.setBackgroundColor(Color.BLUE);
             return true;
-        }else if(item.getItemId() == R.id.item_bg_green){
+        } else if (item.getItemId() == R.id.item_bg_green) {
             linear.setBackgroundColor(Color.GREEN);
             return true;
-        }else if(item.getItemId() == R.id.item_btn_rotation){
-//            btnAngle += 45;
-            btnButton.setRotation(btnButton.getRotation()+45);
+        } else if (item.getItemId() == R.id.item_btn_rotation) {
+            btnButton.setRotation(btnButton.getRotation() + 45);
             return true;
-        }else if(item.getItemId() == R.id.item_bg_zoonin){
+        } else if (item.getItemId() == R.id.item_btn_zoomin) {
             btnButton.setScaleX(2);
             return true;
-        }else if(item.getItemId() == R.id.item_bg_basic){
+        } else if (item.getItemId() == R.id.item_btn_basic) {
             btnButton.setRotation(0);
             btnButton.setScaleX(1);
-//            btnAngle = 0;
             return true;
         }
-
         return false;
     }
 }
